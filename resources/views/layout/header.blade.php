8964fab45ef96499e6a691bd039b88b4d68a0e6d
DevAbdelrahmanSayed <devabdelr2hman@gmail.com>
2023-08-28 03:05:06 +0300

    first commit

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--
+      - primary meta tags
+    -->
+    <title>Adex</title>
+    <meta name="title" content="Adex">
+    <meta name="description" content="This is a business agency html template made by codewithsadee">
+
+    <!--
+      - favicon
+    -->
+    <link rel="shortcut icon" href="{{asset('assets/images/avicon.svg')}}" type="image/svg+xml">
+
+    <!--
+      - google font link
+    -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@500;700&display=swap" rel="stylesheet">
+
+    <!--
+      - custom css link
+    -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    <!--
+    - preload images {{ asset('img/favicon.ico') }}
+    -->
+    <link rel="preload" as="image" href="{{ asset('assets/images/hero-bg.jpg') }}">
+    <link rel="preload" as="image" href="{{ asset('assets/images/hero-slide-1.jpg') }}">
+    <link rel="preload" as="image" href="{{ asset('assets/images/hero-slide-2.jpg') }}">
+    <link rel="preload" as="image" href="{{ asset('assets/images/hero-slide-3.jpg') }}">
+
+
+
+
+</head>
---
+++ resources/views/layout/nav.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar" data-navbar>
+    <div class="navbar-top">
+        <a href="#" class="logo"><img src="{{asset('assets/images/logo-light.png')}}" width="74" height="24" alt="Adex home"></a>
+        <button class="nav-close-btn" aria-label="close menu" data-nav-toggler><ion-icon name="close-outline" aria-hidden="true"></ion-icon></button>
+    </div>
+    <ul class="navbar-list">
+        <li><a href="#" class="navbar-link">Home</a></li>
+        <li><a href="#" class="navbar-link">Join the program</a></li>
+        <li><a href="#" class="navbar-link">Why join aljabri partners ?</a></li>
+        <li><a href="#" class="navbar-link">Dividend system</a></li>
+        <li><a href="#" class="navbar-link">Additional rewards</a></li>
+        <li><a href="#" class="navbar-link">Terms and Condition</a></li>
+        <li><a href="#" class="navbar-link">Call us</a></li>
+    </ul>
+    <div class="wrapper">
+        <a href="mailto:info@email.com" class="contact-link">info@email.com</a>
+        <a href="tel:001234567890" class="contact-link">00 (123) 456 78 90</a>
+    </div>
+</nav>
+<a href="#" class="btn btn-primary">Login</a>
